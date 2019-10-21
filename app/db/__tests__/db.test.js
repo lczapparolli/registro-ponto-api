76ebb02e9a1bc1ebffd8c890257d6b7dc59d2a31
Luis Carlos Zapparolli Neto <lczapparolli@gmail.com>
2019-10-20 23:36:40 -0300

    Creating DB class

---
+++ app/db/__tests__/db.test.js
@@ -0,0 +1,9 @@
+//Tested modules
+const db = require('../db');
+
+describe('DB module', () => {
+    it('should have a "database" property', () => {
+        expect(db).not.toBeNull();
+        expect(db.database).not.toBeNull();
+    });
+});
---
+++ app/db/db.js
@@ -0,0 +1,28 @@
+//Libs
+const MongoClient = require('mongodb').MongoClient;
+
+//Constants
+const url = 'mongodb://localhost:27017'; 
+const dbName = 'registro-ponto';
+
+//Symbols
+const mongoDatabase = Symbol('mongoDatabase');
+
+class db {
+    constructor() {
+        MongoClient.connect(url, (err, client) => {
+            if (err) {
+                throw err;
+            } else {
+                this[mongoDatabase] = client.db(dbName);
+            }
+        });
+    }
+
+    //Properties
+    get database() {
+        return this[mongoDatabase];
+    }
+}
+
+module.exports = new db();
